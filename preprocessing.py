898060b02d3830162517c59e57d9d94db62653ca
Saaketh Jasti <saakethkjasti@outlook.com>
2021-04-11 19:52:45 -0500

    added basic preprocessing file

---
+++ preprocessing.py
@@ -0,0 +1,13 @@
+# imports
+
+
+# Import all the files
+import os
+
+for fname in os.listdir(os.path.join('./', 'landscapes')):
+    pass
+
+# Split the data into train and test
+
+
+# Transform images into YUV and keep Y as X-set and UV as Y-set
